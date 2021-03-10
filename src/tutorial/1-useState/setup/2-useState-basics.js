26c9b9d17695f4861f628312be4bb73f96a65b11
Jugo-JS <jugoslavsubotic@gmail.com>
2021-03-10 10:53:49 +0100

    useState hook

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
+// useState - function - name import
+// hooks starts with use
+// component name must be uppercase
+// must be in the function/component body
+// cannot call conditionaly
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [title, setTitle] = useState('random title');
+
+  const handleClick = () => {
+    if(title === 'random title') {
+      setTitle('hello world');
+    } else {
+      setTitle('random title');
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <h1>{title}</h1>
+      <button className='btn' onClick={handleClick}>Change Title</button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
