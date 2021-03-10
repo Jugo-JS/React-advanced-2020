43f4196ed50931769712e722e0f005c278a195e6
Jugo-JS <jugoslavsubotic@gmail.com>
2021-03-10 12:16:09 +0100

    practicing useState hook

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,28 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] =  React.useState(data);
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        const removeItem = (id) => {
+          let newPeople = people.filter((person) => {
+            return person.id !== id;
+          })
+          console.log(newPeople);
+          setPeople(newPeople);
+        }
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={(() => removeItem(id))}>remove item</button>
+          </div>
+        );
+      })}
+      <button className='btn' onClick={() => setPeople([])}>clear items</button>
+    </>
+  );
 };
 
 export default UseStateArray;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  
+  const resetValue = () => {
+    setValue(0);
+  }
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1); ***setValue is synchronisc and always look start value (which is 0)
+      setValue((prevState) => {
+        return prevState + 1;
+      })
+    }, 2000);
+  }
+
+  return (
+    <>
+      <section style={{margin: '4rem 0'}}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={() => setValue(value -1)}>decrease</button>
+        <button className='btn' onClick={resetValue}>reset</button>
+        <button className='btn' onClick={() => setValue(value + 1)}>increase</button>
+      </section>
+      <section style={{margin: '4rem 0'}}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>increase</button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
