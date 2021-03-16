f155dc01d2795915467cde65216e0c7b6e495f63
Jugo-JS <jugoslavsubotic@gmail.com>
2021-03-16 12:06:26 +0100

    practicin forms with controlled inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,72 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const[firstName, setFirstName] = useState('');
+  const[email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(firstName && email) {
+      const person = {
+        id: new Date().getTime().toString(),
+        firstName: firstName,
+        email: email
+      }
+      setPeople((prevState) => {
+        return [...prevState, person];
+      });
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('empty values');
+    }
+  }
+
+  console.log(people);
+
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name : </label>
+            <input 
+              type='text' 
+              id='firstName' 
+              name='firstName' 
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+            >
+            </input>
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email : </label>
+            <input 
+              type='text' 
+              id='email' 
+              name='email' 
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            >
+            </input>
+          </div>
+          <button type='submit'>add person</button>
+        </form>
+        {
+          people.map((person) => {
+            const { id, firstName, email } = person;
+            return (
+              <div className='item' key={id}>
+                <h4>{firstName}</h4>
+                <p>{email}</p>
+              </div>
+            );
+          })
+        }
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
